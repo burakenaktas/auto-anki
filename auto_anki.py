27fc38094050151cb011d971a2030a045e028989
Burak <business.burakaktas@gmail.com>
2022-12-25 04:56:39 +0300

    Read list, some configs, and beginning of german language

---
+++ auto_anki.py
@@ -0,0 +1,49 @@
+import pyautogui as events
+import time
+
+### Config ###
+seperator = " - "
+card_file = open("card_file.txt", "r")
+word_array = []
+word_part_range = 3
+
+## Deck Config ##
+front_field_px = [893, 154]
+back_field_px = [867, 226]
+add_field_px = [671, 1054]
+color_field_px = [285, 87]
+color_red_px = [242, 638]
+color_green_px = [269, 638]
+color_blue_px = [301, 638]
+color_ok_button = [611, 730]
+
+## Language Config ##
+isEnglish = False  # If it is not English that means it is German
+
+### Word Array Preparing ###
+for card in card_file:
+    splitted_array = card.split(seperator)
+    last_one_corrected_splitted_array = [
+        splitted_array[0], splitted_array[1], splitted_array[2].rsplit("\n")[0]]
+    word_array.append(last_one_corrected_splitted_array)
+
+# To check word_array
+# print(word_array)
+
+time.sleep(3)
+print(events.position())
+
+### After Empty Deck Opened ###
+
+for word in word_array:
+    for index in range(word_part_range):
+        current_word = word[index]
+        if index == 0:
+            if 'der' == current_word[:3]:
+                events.click(color_field_px[0], color_field_px[1])
+                events.click(color_blue_px[0], color_blue_px[1])
+                events.click(color_ok_button[0], color_ok_button[1])
+
+# events.click(color_field_px[0], color_field_px[1])
+# events.write(deck_name)
+# events.click(ok_button_px[0], ok_button_px[1])
